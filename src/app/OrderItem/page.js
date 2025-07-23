060c49f4c57c77dc196f40e268450f839ae99d14
willymusyoka <willykyeni@gmail.com>
2025-07-23 20:43:58 +0300

    Adds handleSubmit edit changes function

--- src/app/OrderItem/page.js
+++ src/app/OrderItem/page.js
@@ -2,13 +2,28 @@ import { useState } from "react";
 import ordersForm from "../AddOrder/page";
 // import updateOrder from Mike
 
-function EditOderForm() {
+function EditOderForm({ order, onclose, onOrderUpdated }) {
     const [customerName, setCustomerName] = useState('');
     const [clothType, setClothType] = useState('');
     const [dateSubmitted, setDateSubmitted] = useState('');
     const [dateCompleted, setDateCompleted] = useState('');
     const [price, setPrice] = useState('');
     const [status, setStatus] = useState(ordersForm.status);
+
+   // handle edit update
+    const hundleSubmit = async (e) => {
+        e.preventDefault();
+        await updateOrder(order.id, {
+            customerName,
+            clothType,
+            dateSubmitted,
+            dateCompleted,
+            price,
+            status,
+        });
+        onOrderUpdated();
+        onclose();
+    };
 
 }
 
