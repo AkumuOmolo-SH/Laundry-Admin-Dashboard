37e507f67f65e702cd536f40a360d26043d6c06a
AkumuOmolo <akumu.omoloh@gmail.com>
2025-07-24 08:23:19 +0300

    Adds copyright and header changes

---
+++ components/Header.jsx
@@ -0,0 +1,11 @@
+import react from "react";
+
+function Header() {
+  return (
+    <div>
+      <h2 className="text-center font-medium text-[22] text-white mt-50px ">&copy;Kneat Laundries</h2>
+    </div>
+  );
+}
+
+export default Header;
