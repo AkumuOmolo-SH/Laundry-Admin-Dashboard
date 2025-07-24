93d4ddf4f4c2090b5f3ac32f1b78f92b9d120e1f
AkumuOmolo <akumu.omoloh@gmail.com>
2025-07-24 13:02:52 +0300

    Adds blur to background and changes text top margin

--- components/Background.jsx
+++ components/Background.jsx
@@ -2,8 +2,8 @@ import react from "react";
 
 export default function Background() {
   return (
-    <div 
-    className="absolute inset-0 bg-gradient-to-r from-white/60 via-white/50 to-white/70"
+    <div
+      className="absolute inset-0 blur-sm"
       style={{
         backgroundImage: "url('/background.png')",
         backgroundSize: "cover",
--- components/Header.jsx
+++ components/Header.jsx
@@ -1,11 +1,11 @@
-import react from "react";
+import React from "react";
 import Image from "next/image";
 import Navbar from "./Navbar";
 
 function Header() {
  return (
     <div className="flex items-center justify-between px-8 py-4">
-      <div className="w-50 h-10 relative mix-blend-multiply">
+      <div className="w-48 h-10 relative mix-blend-multiply">
         <Image
           src="/logo.png" 
           alt="Business Logo"
